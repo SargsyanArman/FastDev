99a3ca7f14d6f991198cc4b2ede1734114ba77bc
Arman <11arman@bk.ru>
2024-10-13 05:03:53 -0800

    Created FireBase.js file

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDBjt2vRGkc4D6FeCXuyjdTdDHUABNj0Eg",
+    authDomain: "fastdev-b2278.firebaseapp.com",
+    projectId: "fastdev-b2278",
+    storageBucket: "fastdev-b2278.appspot.com",
+    messagingSenderId: "142079107016",
+    appId: "1:142079107016:web:50024c0ee4d7363d432afd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
