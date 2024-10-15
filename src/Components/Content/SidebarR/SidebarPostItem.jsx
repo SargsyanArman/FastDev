b4dd63f5ce0e7e4dd965026860a80cccc92f3906
Arman <11arman@bk.ru>
2024-10-15 13:22:48 -0800

    Sidebar right links to posts created

--- src/Components/Content/SidebarR/SidebarPostItem.jsx
+++ src/Components/Content/SidebarR/SidebarPostItem.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import go from '../../../images/go.webp'
+import React, { useEffect, useState } from 'react';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { db } from '../../../firebase';
+import { Link } from 'react-router-dom';
+import go from '../../../images/go.webp';
 
-const SidebarPostItem = ({ icons }) => {
+const SidebarPostItem = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, 'posts'), (snapshot) => {
+      const fetchedPosts = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setPosts(fetchedPosts);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className='mt-5 flex w-full flex-col gap-[29px]'>
-      {icons.map((item, index) => (
-        <a key={index} href='#' className='flex cursor-pointer items-center justify-between gap-2'>
-          <img src={go} alt="Go logo" className='h-[50px] w-[50px] rounded-[10px] hover:scale-105 ' />
-          <span className='text-[14px] leading-[18.2px] text-zinc-700 hover:text-red-500 '>Mastering Concurrency in Go: A Comprehensive Guide to Goroutines and Channels</span>
-          {item}
-        </a>
+      {posts.slice(0, 4).map((post) => (
+        <Link key={post.id} to={`/posts/${post.id}`} className='flex cursor-pointer items-center justify-between gap-2'>
+          <div className='flex gap-2 items-center'>
+            <img src={post.imageUrl || go} alt="Post logo" className='h-[50px] w-[50px] rounded-[10px] hover:scale-105' />
+            <span className='text-[14px] leading-[18.2px] text-zinc-700 hover:text-red-500'>{post.title}</span>
+          </div>
+          <i className="fa-solid fa-arrow-right-long sidebar-right-btnRight flex"></i>
+        </Link>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default SidebarPostItem
-// flex cursor-pointer items-center justify-between gap-2
-// <a key={index} className='flex justify-center gap-[10px] w-[298px] h-[50px] text-[14px] pr-5 mt-7 rounded-[10px]'>
+export default SidebarPostItem;
