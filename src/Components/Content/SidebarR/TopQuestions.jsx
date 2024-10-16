ecffd7b26c7b40c53b61d3fd66f0c2c3b86769cb
Arman <11arman@bk.ru>
2024-10-16 12:41:28 -0800

    question right side items has been created

--- src/Components/Content/SidebarR/TopQuestions.jsx
+++ src/Components/Content/SidebarR/TopQuestions.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import TopQuestionsItem from './TopQuestionsItem'
+import React, { useEffect, useState } from 'react';
+import { db } from '../../../firebase'
+import { collection, getDocs } from 'firebase/firestore';
+import TopQuestionsItem from './TopQuestionsItem';
 
 const TopQuestions = () => {
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() => {
+        const fetchQuestions = async () => {
+            const questionsCollection = collection(db, 'questions');
+            const questionSnapshot = await getDocs(questionsCollection);
+            const questionList = questionSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setQuestions(questionList);
+        };
+
+        fetchQuestions();
+    }, []);
+
     return (
-        <div className='mt-14 '>
+        <div className='mt-14'>
             <h3 className='text-zinc-900 font-bold text-[20px]'>Top Questions</h3>
             <div className='mt-5 flex w-full flex-col gap-[30px]'>
-                <TopQuestionsItem />
-                <TopQuestionsItem />
-                <TopQuestionsItem />
-                <TopQuestionsItem />
+                {questions.slice(0, 4).map(question => (
+                    <TopQuestionsItem key={question.id} question={question} />
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default TopQuestions
+export default TopQuestions;
--- src/Components/Content/SidebarR/TopQuestionsItem.jsx
+++ src/Components/Content/SidebarR/TopQuestionsItem.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const TopQuestionsItem = () => {
+const TopQuestionsItem = ({ question }) => {
   return (
-    <a href='#' className='flex cursor-pointer items-center justify-between gap-7'>
-        <span className='text-[14px] leading-[18.2px] text-zinc-700 hover:text-red-500'>
-            Is there a way in Golang to define a generic function type with varargs that can take any function as input?
-        </span>
-        <span> <i className="fa-solid fa-arrow-right-long sidebar-right-btnRight"> </i></span>
-    </a>
-  )
-}
+    <Link to={`/question/${question.id}`} className='flex cursor-pointer items-center justify-between gap-7'>
+      <span className='text-[14px] leading-[18.2px] text-zinc-700 hover:text-red-500'>
+        {question.title}
+      </span>
+      <span>
+        <i className="fa-solid fa-arrow-right-long sidebar-right-btnRight"></i>
+      </span>
+    </Link>
+  );
+};
 
-export default TopQuestionsItem
+export default TopQuestionsItem;
