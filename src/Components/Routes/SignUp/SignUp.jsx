45cf145fc5506a13541e15c607504454c475ecd2
Ann Sargsyan <annsargsyan2707@gmail.com>
2024-12-28 16:07:20 +0400

    dark mode is without  mt-6

--- src/Components/Routes/SignUp/SignUp.jsx
+++ src/Components/Routes/SignUp/SignUp.jsx
@@ -93,7 +93,7 @@ const SignUp = () => {
                                 onChange={handleChange}
                                 autoComplete="email"
                                 required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset p-2 sm:text-sm sm:leading-6 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset p-2 sm:text-sm sm:leading-6 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400npm "
                             />
                         </div>
                     </div>
